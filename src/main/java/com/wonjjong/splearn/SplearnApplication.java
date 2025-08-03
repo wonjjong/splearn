28587dcb7a286c8f3ddef16ffdf9a48241aab26b
wonjjong <wonjjong.dev@gmail.com>
2025-08-04 00:10:55 +0900

    splearn 프로젝트 생성

---
+++ src/main/java/com/wonjjong/splearn/SplearnApplication.java
@@ -0,0 +1,13 @@
+package com.wonjjong.splearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wonjjong/splearn/SplearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonjjong.splearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
